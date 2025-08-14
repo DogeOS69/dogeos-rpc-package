450c407ee94093380185093d4043dc33029aaad6
paul-unifra <paul@unifra.io>
2025-08-15 02:49:36 +0800

    Update Docker Compose and environment files for L2Geth and Celestia integration
    - Added `depends_on` for `l1-interface` in `docker-compose.yml` to ensure proper service startup order.
    - Updated `l2geth.env` to set the correct contract deployment block and modified the peer list for improved connectivity.
    - Enhanced `entrypoint.sh` to include the new DA Blob Beacon Node configuration for L2Geth, ensuring compatibility with the latest architecture.

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -25,6 +25,7 @@ exec geth \
     $L2GETH_CCC_FLAG --ccc.numworkers "$L2GETH_CCC_NUMWORKERS" \
     $METRICS_FLAGS \
     --scroll-mpt \
+    --rollup.verify --da.blob.beaconnode "$L2GETH_DA_BLOB_BEACON_NODE" \
     --gcmode archive \
     --cache.noprefetch --cache.snapshot=0 \
     --snapshot=false \
