92c57d6d42a362f98b0d5c85fcdc0f1d9c1a2a10
Morty <70688412+yiweichi@users.noreply.github.com>
2025-11-11 02:31:14 +0800

    feat: add reth node (#6)
    * Config / env for testnet
    
    * Update l1-interface tag
    
    * feat: add reth node
    
    * update new testnet info
    
    * feat: update images
    
    * feat: update images
    
    * fix typo
    
    * revert celestia changes
    
    * fix: l1-interface genesis mount
    
    * fix: l2reth
    
    ---------
    
    Co-authored-by: Alex Lewis <alexvlewis@gmail.com>

---
+++ scripts/l2reth_entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+export RUST_LOG=sqlx=off,scroll=trace,reth=trace,rollup=trace,info
+
+exec rollup-node node --chain /l2reth/genesis/genesis.json --datadir=/l2reth --metrics=0.0.0.0:6060 --network.scroll-wire=true --network.bridge=true  \
+  --http --http.addr=0.0.0.0 --http.port=8545 --http.corsdomain "*" --http.api admin,debug,eth,net,trace,txpool,web3,rpc,reth,ots,flashbots,miner,mev \
+  --ws --ws.addr=0.0.0.0 --ws.port=8546 --ws.api admin,debug,eth,net,trace,txpool,web3,rpc,reth,ots,flashbots,miner,mev \
+  --log.stdout.format log-fmt -vvv \
+  --txpool.pending-max-count=1000 \
+  --builder.gaslimit=30000000 \
+  --rpc.max-connections=5000 \
+  --trusted-peers="$L2GETH_PEER_0" --trusted-peers="$L2GETH_PEER_1" \
+  --engine.sync-at-startup false \
+  --l1.url "$L2RETH_L1_ENDPOINT" \
+  --blob.beacon_node_urls="$L2RETH_DA_BLOB_BEACON_NODE" \
+  --network.valid_signer="$L2RETH_VALID_SIGNER"
