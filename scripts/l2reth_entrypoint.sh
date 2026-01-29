d3171e79bb88bb9648396167486969cc2cb66a26
shu-unifra <shu@unifra.io>
2026-01-29 15:48:53 +0900

    update devnet rpc package configs

--- scripts/l2reth_entrypoint.sh
+++ scripts/l2reth_entrypoint.sh
@@ -5,6 +5,15 @@ export RUST_LOG=sqlx=off,scroll=trace,reth=trace,rollup=trace,info
 
 #Available API
 #admin,debug,eth,net,trace,txpool,web3,rpc,reth,ots,flashbots,miner,mev
+# Parse L2GETH_PEER_LIST
+PEER_FLAGS=""
+if [ -n "$L2GETH_PEER_LIST" ]; then
+    CLEAN_LIST=$(echo "$L2GETH_PEER_LIST" | sed 's/[][]//g' | sed 's/"//g')
+    for peer in $(echo "$CLEAN_LIST" | tr ',' ' '); do
+        PEER_FLAGS="$PEER_FLAGS --trusted-peers=$peer"
+    done
+fi
+
 exec rollup-node node --chain /l2reth/genesis/genesis.json --datadir=/l2reth --metrics=0.0.0.0:6060 --network.scroll-wire=true --network.bridge=true  \
   --http --http.addr=0.0.0.0 --http.port=8545 --http.corsdomain "*" --http.api eth,net,web3,debug,trace \
   --ws --ws.addr=0.0.0.0 --ws.port=8546 --ws.api eth,net,web3,debug,trace \
@@ -12,8 +21,8 @@ exec rollup-node node --chain /l2reth/genesis/genesis.json --datadir=/l2reth --m
   --txpool.pending-max-count=1000 \
   --builder.gaslimit=30000000 \
   --rpc.max-connections=5000 \
-  --trusted-peers="$L2GETH_PEER_0" --trusted-peers="$L2GETH_PEER_1" \
+  $PEER_FLAGS \
   --engine.sync-at-startup false \
-  --l1.url "$L2RETH_L1_ENDPOINT" \
-  --blob.beacon_node_urls="$L2RETH_DA_BLOB_BEACON_NODE" \
+  --l1.url "http://l1-interface:8545" \
+  --blob.beacon_node_urls="http://l1-interface:5052" \
   --network.valid_signer="$L2RETH_VALID_SIGNER"
